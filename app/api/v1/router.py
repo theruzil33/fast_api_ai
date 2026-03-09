fdbbb135dd5e585dc2562eee3f46324606362fad
Рузиль <ruzil33@MacBook-Air-Ruzil.local>
2026-03-09 12:21:17 +0500

    fix

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import health, photos
+from app.api.v1.endpoints import health
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, prefix="/health", tags=["health"])
-api_router.include_router(photos.router, prefix="/photos", tags=["photos"])
